06595ea678b1c3270052d968235c3c5309a92d51
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-28 16:16:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,24 @@
 class Dog
   def initialize(name)
+    #stored within a @name and @breed instance var
     @name = name
+    # @breed = breed
   end
   
   def name=(name)
     @name = name
   end
-
+  
   def name
     @name
   end
+  
+  # attr_accessor:breed
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+  
+  # def breed
+  #   @mutt
+  # end
 end
