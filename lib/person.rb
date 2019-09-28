d9bf169f3c1b97472b08878b7fa0e4e5592dc9b9
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-28 16:16:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,25 +1,13 @@
 class Person
   def initialize(name)
-    #stored within a @name and @breed instance var
     @name = name
-    # @breed = breed
   end
   
   def name=(name)
     @name = name
   end
-  
+
   def name
     @name
   end
-  
-  # attr_accessor:breed
-  # def breed=(breed)
-  #   @breed = breed
-  # end
-  
-  # def breed
-  #   @mutt
-  # end
-  
 end
